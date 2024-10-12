2659c2993529a5a81865e761a9ada00888d27ea1
manhminh <manhminh2601@gmail.com>
2024-10-12 21:43:24 +0700

    add send email service

---
+++ notification-service/src/main/java/com/devintel/notification/dto/request/SendEmailRequest.java
@@ -0,0 +1,15 @@
+package com.devintel.notification.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SendEmailRequest {
+    RecipientRequest to;
+    String subject;
+    String htmlContent;
+}
